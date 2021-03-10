03bd463a1360d19f339885af15aaf43acaa40c6c
i007c <dr007cc@gmail.com>
2021-03-11 03:11:34 +0330

    init Projects Models

--- Team_00/Projects/models.py
+++ Team_00/Projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(default="No Description")
+    video = models.FileField(upload_to="Projects/video/")
+    thumbnail = models.FileField(upload_to="Projects/thumbnail/")
+    
+    def delete(self, using=None, keep_parents=False):
+        self.video.storage.delete(self.video.name)
+        self.thumbnail.storage.delete(self.thumbnail.name)
+        super().delete()
+
+    def __str__(self):
+        return self.name
