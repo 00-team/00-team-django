ad5e16869bb809146db8b474a0c4b483df78a40f
i007c <dr007cc@gmail.com>
2021-03-10 22:04:56 +0330

    replace all Quotation with double Quotation and add Home urls

--- Team_00/Home/apps.py
+++ Team_00/Home/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
-    name = 'Home'
+    name = "Home"
--- Team_00/Projects/apps.py
+++ Team_00/Projects/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ProjectsConfig(AppConfig):
-    name = 'Projects'
+    name = "Projects"
