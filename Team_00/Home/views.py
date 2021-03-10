3805d9bd974641d6d4541860e800631926c0e826
i007c <dr007cc@gmail.com>
2021-03-11 03:11:10 +0330

    make view for Home and Projects

--- Team_00/Home/views.py
+++ Team_00/Home/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
+from django.template import loader
 
+from Projects.models import Project
+
+from django.views.decorators.http import require_GET
+
+
+@require_GET
 def index(request):
-    return HttpResponse("index.")
+    last_project = Project.objects.last()
+    c = {}
+    if last_project:
+        c = {
+            "last_project": {
+                "name": last_project.name,
+                "description": last_project.description,
+                "video": last_project.video.url,
+                "thumbnail": last_project.thumbnail.url
+            }
+        }
+
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render(c, request))
--- Team_00/Projects/views.py
+++ Team_00/Projects/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+from django.template import loader
+
+from Projects.models import Project
+
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+def projects(request):
+    all_projects = Project.objects.all()
+    c = {}
+    if all_projects:
+        a = []
+        for p in all_projects:
+            a.append({
+                "name": p.name,
+                "description": p.description,
+                "video": p.video.url,
+                "thumbnail": p.thumbnail.url,
+                "index": p.id
+            })
+        
+        c["projects"] = a
+        del a
+
+    template = loader.get_template("projects.html")
+    return HttpResponse(template.render(c, request))
