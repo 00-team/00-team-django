d7daff7a38b8997e4e1bd611e3445c139476912d
i007c <dr007cc@gmail.com>
2021-03-11 03:33:20 +0330

    replace sample-settings to settings

--- Team_00/Team_00/sample-settings.py
+++ Team_00/Team_00/sample-settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6pp4w21g$!oo1y6p(rlgi^t&do1zd30%p+d9fg)+j2evhwgle^"
+SECRET_KEY = ""
 
 DEBUG = True
 
