c0f8fa827ad95e36f201417a58fd2c64df975b8c
i007c <dr007cc@gmail.com>
2021-03-13 17:50:06 +0330

    make errors view

--- Team_00/Home/views.py
+++ Team_00/Home/views.py
@@ -25,3 +25,29 @@ def index(request):
 
     template = loader.get_template("index.html")
     return HttpResponse(template.render(c, request))
+
+
+def error_handler(request, code, title, description):
+    template = loader.get_template("error.html")
+    c = {"error": {
+            "code": code,
+            "title": title,
+            "description": description,
+        }}
+    return HttpResponse(template.render(c, request), status=404)
+
+
+def error_400(request, exception=None):
+    return error_handler(request, code=400, title="Bad Request", description=f"Request was not polite enough")
+
+
+def error_403(request, exception=None):
+    return error_handler(request, code=403, title="Permission Denied", description=f"Your client does not have permission to get URL \"{request.path}\" from this server. That’s all we know.")
+
+
+def error_404(request, exception=None):
+    return error_handler(request, code=404, title="Not Found", description=f"The requested URL \"{request.path}\" was not found on this server. That’s all we know.")
+
+
+def error_500(request, exception=None):
+    return error_handler(request, code=500, title="Server Error", description=f"Internal Server Error")
