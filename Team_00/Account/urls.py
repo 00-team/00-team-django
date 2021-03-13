b054896390a08adab70e51af57af7ef675104423
i007c <dr007cc@gmail.com>
2021-03-13 12:00:05 +0330

    register account urls: login, logout, account, ..

--- Team_00/Account/urls.py
+++ Team_00/Account/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/google/", views.authorize, name="google-authorize"),
-    path("login/google_callback/", views.google_callback, name="google-callback"),
+    path("", views.account, name="account view"),
+    path("login/google/", views.authorize, name="google authorize"),
+    path("login/google_callback/", views.google_callback, name="google callback"),
+    path("logout/", views.logout_user, name="logout user"),
 ]
