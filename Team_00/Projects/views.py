e5afaf35d0ff758f595dfb0cad3a9550a56e33e5
i007c <dr007cc@gmail.com>
2021-03-16 00:27:05 +0330

    better projects display

--- Team_00/Projects/views.py
+++ Team_00/Projects/views.py
@@ -11,21 +11,41 @@ from django.views.decorators.http import require_GET
 
 @require_GET
 def projects(request):
-    all_projects = Project.objects.all()
+    star_projects = Project.objects.order_by("-stars")[:6]
+    time_projects = Project.objects.order_by("-date_start")[:6]
     c = {}
-    if all_projects:
-        a = []
-        for p in all_projects:
-            a.append({
+    if star_projects:
+        s = []
+        for p in star_projects:
+            s.append({
                 "name": p.name,
                 "description": p.description,
                 "video": p.video.url,
                 "thumbnail": p.thumbnail.url,
-                "index": str(p.slug)
+                "index": p.slug,
+                "date_start": p.date_start.strftime("%Y-%m-%d"),
+                "stars": p.stars,
+                "language": p.language,
+                "workspace": p.workspace
+            })
+    
+    if time_projects:
+        t = []
+        for p in time_projects:
+            t.append({
+                "name": p.name,
+                "description": p.description,
+                "video": p.video.url,
+                "thumbnail": p.thumbnail.url,
+                "index": p.slug,
+                "date_start": p.date_start.strftime("%Y-%m-%d"),
+                "stars": p.stars,
+                "language": p.language,
+                "workspace": p.workspace
             })
         
-        c["projects"] = a
-        del a
+        c["star_projects"] = s
+        c["time_projects"] = t
 
     template = loader.get_template("projects.html")
     return HttpResponse(template.render(c, request))
