6bbfd68be3e93a5bd29d4402d0bcbc926a2f92c9
i007c <dr007cc@gmail.com>
2021-03-15 10:11:32 +0330

    add test for Projects

--- Team_00/Projects/tests.py
+++ Team_00/Projects/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        Project.objects.create(
+            name="Project 1",
+            description="roar",
+            video = open("video.mp4", "w"),
+            thumbnail = open("thumbnail.png", "w")
+        )
