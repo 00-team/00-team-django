b92a330a17dc10bab6045e8ce4af5e113998a87a
i007c <dr007cc@gmail.com>
2021-03-16 21:57:09 +0330

    add all project and this need a filter

--- Team_00/Projects/views.py
+++ Team_00/Projects/views.py
@@ -14,6 +14,7 @@ from Home.views import error_404
 def projects(request):
     star_projects = Project.objects.order_by("-stars")[:6]
     time_projects = Project.objects.order_by("-date_start")[:6]
+    all_projects = Project.objects.all()
     c = {}
     if star_projects:
         s = []
@@ -44,9 +45,25 @@ def projects(request):
                 "language": p.language,
                 "workspace": p.workspace
             })
+    
+    if all_projects:
+        a = []
+        for p in all_projects:
+            a.append({
+                "name": p.name,
+                "description": p.description,
+                "video": p.video.url,
+                "thumbnail": p.thumbnail.url,
+                "slug": p.slug,
+                "date_start": p.date_start.strftime("%Y-%m-%d"),
+                "stars": p.stars,
+                "language": p.language,
+                "workspace": p.workspace
+            })
         
         c["star_projects"] = s
         c["time_projects"] = t
+        c["all_projects"] = a
 
     template = loader.get_template("projects.html")
     return HttpResponse(template.render(c, request))
@@ -54,9 +71,23 @@ def projects(request):
 
 @require_GET
 def project(request, slug):
-    if Project.objects.filter(slug=slug).exists():
-        p = Project.objects.get(slug=slug)
-    else:
-        return error_404(request, exception="X")
+    if not Project.objects.filter(slug=slug).exists():
+        return error_404(request)
+        
+    p = Project.objects.get(slug=slug)
+    c = {
+        "p": {
+            "name": p.name,
+            "description": p.description,
+            "video": p.video.url,
+            "thumbnail": p.thumbnail.url,
+            "slug": p.slug,
+            "date_start": p.date_start.strftime("%Y-%m-%d"),
+            "stars": p.stars,
+            "language": p.language,
+            "workspace": p.workspace
+        }
+    }
 
-    return HttpResponse(p)
+    template = loader.get_template("project.html")
+    return HttpResponse(template.render(c, request))
