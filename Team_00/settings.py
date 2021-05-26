ba5113de37314adcb25750bd40ee37ddb948dba6
i007c <dr007cc@gmail.com>
2021-05-26 10:21:45 +0430

    add auto field

--- Team_00/settings.py
+++ Team_00/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/s/"
 # STATIC_ROOT = BASE_DIR / "Team_00/static"
 
 STATICFILES_DIRS = [
@@ -133,4 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / "Team_00/media"
-MEDIA_URL = "/media/"
+MEDIA_URL = "/m/"
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
