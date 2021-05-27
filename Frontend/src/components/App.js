ef7139e6282b3592b096248a767970d11c286d84
i007c <dr007cc@gmail.com>
2021-05-27 14:19:44 +0430

    add react stuff

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <h1>apps</h1>
+        </div>
+    )
+}
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById('root'))
