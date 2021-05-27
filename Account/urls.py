803275a7c887e9c4280340cbbdc70f41986c45d1
i007c <dr007cc@gmail.com>
2021-05-27 23:42:06 +0430

    debug a url

--- Account/urls.py
+++ Account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("login/", views.login_user, name="Login"),
     path("login/google/", views.authorize, name="google authorize"),
     path("login/google_callback/", views.google_callback, name="google callback"),
-    path("logout", views.logout_user, name="logout"),
+    path("logout/", views.logout_user, name="logout"),
     path("change_password/", views.change_password, name="Change Password"),
 ]
