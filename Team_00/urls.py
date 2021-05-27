770080e9a1e5781c382af21a011c55202b09eb77
i007c <dr007cc@gmail.com>
2021-05-27 23:49:05 +0430

    better urls for api and react

--- Team_00/urls.py
+++ Team_00/urls.py
@@ -8,8 +8,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('projects/', include('Projects.urls')),
-    path('account/', include('Account.urls')),
+    path('api/projects/', include('Projects.urls')),
+    path('api/account/', include('Account.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('<path:resource>', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
