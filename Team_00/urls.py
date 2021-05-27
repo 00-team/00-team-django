bf1d88886f6d11feda5acb5c98e499d211dd9932
i007c <dr007cc@gmail.com>
2021-05-27 14:21:11 +0430

    link main page to react app

--- Team_00/urls.py
+++ Team_00/urls.py
@@ -4,8 +4,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.generic import TemplateView
+
+
 urlpatterns = [
-    path("projects/", include("Projects.urls")),
-    path("account/", include("Account.urls")),
-    path("admin/", admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('projects/', include('Projects.urls')),
+    path('account/', include('Account.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
