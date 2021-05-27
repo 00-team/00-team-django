8543c21939c28de7f4fca3d0b7052e8e02b99555
i007c <dr007cc@gmail.com>
2021-05-27 14:20:33 +0430

    better user model for now

--- Account/models.py
+++ Account/models.py
@@ -8,16 +8,22 @@ import string
 
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    picture = models.URLField()
+    name = models.CharField(max_length=100, default="No Name")
+    picture = models.URLField(null=True, blank=True)
     locale = models.CharField(max_length=10)
     token = models.CharField(max_length=50, null=True, blank=True, unique=True)
 
     def change_token(self):
         try:
             self.token = get_random_string(50, string.ascii_letters + string.digits + "!@#$%^&*")
+            self.save()
         except IntegrityError:
             self.change_token()
+    
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.change_token()
+        super(UserAccount, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
