121301dc6f385d175da9a9a3240a2e3c76b801e6
i007c <dr007cc@gmail.com>
2021-05-29 01:59:20 +0430

    clean code and some new methods like react router and link components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,89 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+
+// components
+import Header from './components/layouts/Header'
+import Home from './components/Home'
+import Account from './components/Account'
+import Error from './components/Error'
+
+var csrfToken = document.currentScript.getAttribute('csrfToken');
 
 
-function App() {
-    // const [user, setUser] = useState({
-    //     username: null,
-    //     name: null,
-    //     email: null,
-    //     picture: null,
-    //     token: null
-    // })
+const App = () => {
+    const [user, setUser] = useState(true);
+    const location = useLocation();
 
-    // useEffect(() => {
-    //     fetch("")
-    //     .then(res => res.json())
-    //     .then(
-    //         (result) => {
-    //             if (result.user) {
-    //                 setUser(result.user)
-    //             } else {
-    //                 setUser(null)
-    //             }
-    //         },
-    //         (error) => {
-    //             setUser(null)
-    //             console.log(error);
-    //         }
-    //     )
-    // }, [])
-    
-    // console.log(user);
-    
+    useEffect(() => {
+        fetch('/api/account/')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setUser(result.user);
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }, [])
+
+
     return (
-        <div>
-            <h1>aps</h1>
-            <Account />
-        </div>
+        <>
+            <Header user={user} />
+
+            <div className='content'>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/account">
+                        <Account user={user} />
+                    </Route>
+                    <Route path="*">
+                        <Error code='404' title='Not Found' description={<>The requested URL <span className='location-path'>{location.pathname}</span> was not found on this server. That’s all we know.</>} />
+                    </Route>
+                </Switch>
+            </div>
+        </>
     )
 }
 
 export default App
 
 
-function Account() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [user, setUser] = useState({});
-    useEffect(() => {
-        fetch("/api/account/")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setUser(result.user);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    }, [])
-    
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        if (user) {
-            return (
-                <div>
-                    <span>{user.name}</span> <br />
-                    <span>{user.username}</span> <br />
-                    <span>{user.email}</span> <br />
-                    <img src={user.picture} />
-                </div>
-            );
-        } else {
-            return (
-                <span>pls login</span>
-            );
-        }
-    }
-
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+)
