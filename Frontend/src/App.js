ed138597ca71c76c3706e7288f8e3f982a874fbf
i007c <dr007cc@gmail.com>
2021-05-30 01:00:53 +0430

    add login route and debug some small errors

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useLocation, Redirect } from 'react-router-dom';
 
 // components
 import Header from './components/layouts/Header'
 import Home from './components/Home'
 import Account from './components/Account'
 import Error from './components/Error'
+import Login from './components/layouts/Login'
 
 var csrfToken = document.currentScript.getAttribute('csrfToken');
 
 
 const App = () => {
-    const [user, setUser] = useState(true);
+    const [user, setUser] = useState({});
     const location = useLocation();
 
     useEffect(() => {
@@ -38,9 +39,15 @@ const App = () => {
                     <Route exact path="/">
                         <Home />
                     </Route>
+
                     <Route path="/account">
-                        <Account user={user} />
+                        <Account />
                     </Route>
+
+                    <Route path="/login" >
+                        {user != true && user ? <Redirect to='/account' /> :<Login />}
+                    </Route>
+                    
                     <Route path="*">
                         <Error code='404' title='Not Found' description={<>The requested URL <span className='location-path'>{location.pathname}</span> was not found on this server. That’s all we know.</>} />
                     </Route>
