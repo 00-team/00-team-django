900bc7bc184328fe74ee67a258a064de34e4e386
i007c <dr007cc@gmail.com>
2021-05-29 15:47:38 +0430

    add some test for user account model

--- Account/tests.py
+++ Account/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client    
+from django.contrib.auth.models import User
+from .models import UserAccount
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('test-user', email='test@example.com', password='th3TestPa$$w0rd')
+        self.ua = UserAccount(user=self.u, nickname='test', locale='en')
+        self.ua.save()
+
+
+    def test_account(self):
+        self.assertEqual(self.ua.nickname, 'test')
+
