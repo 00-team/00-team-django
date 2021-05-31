3c257580f639a14526836d0864840c066fc2769b
i007c <dr007cc@gmail.com>
2021-05-31 04:52:29 +0430

    log of changes, get started project, remove with double click, clean a lot of code

--- Frontend/src/components/Account.js
+++ Frontend/src/components/Account.js
@@ -1,61 +1,113 @@
 import React, { useEffect, useState } from 'react'
-import Button from './layouts/Button'
 import { FiAtSign, FiUser, FiHexagon, FiStar } from 'react-icons/fi'
+import { useHistory } from 'react-router-dom'
 
+import Button from './layouts/Button'
 
 var csrfToken = document.currentScript.getAttribute('csrfToken');
 
+
 const go = (path) => window.location.replace(path);
 
-const logout = () => {
-    fetch('/api/account/logout/', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-            'X-CSRFToken': csrfToken
-        },
-    })
-    .then(res => res.json())
-    .then(
-        (result) => {
-            if (result.success) {
-                go('/')
-            } else {
-                console.log(result.Error);
-            }
-        },
-        (error) => {
-            console.log(error);
-        }
+const Projects = ({ projects, Rstar, history }) => {
+    if (projects.length == 0) return <></>
+
+    return (
+        projects.map((p, i) =>
+            <div key={i} className='project'>
+                <div className='thumbnail' style={p.thumbnail ? { '--ps-bg-img':'url(' + p.thumbnail + ')' } : {}} onDoubleClick={() => {Rstar(p.id)}} >
+                    <span onClick={() => history.push('/project/' + p.slug)} >{p.name}</span>
+                </div>
+                <div className='lw'>
+                    <span className='lang'>{p.lang}</span>
+                    <span className='wspace'>{p.wspace}</span>
+                </div>
+            </div>
+        )
     )
 }
 
+
 const Account = () => {
     const [user, setUser] = useState({});
+    const [projects, setProjects] = useState([]);
+    const history = useHistory();
 
     useEffect(() => {
         fetch('/api/account/')
         .then(res => res.json())
         .then(
-            (result) => {
-                setUser(result.user);
+            (r) => {
+                if (r.user) {
+                    setProjects(r.user.stared_projects)
+                }
+
+                setUser(r.user);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }, [])
+
+
+    const removeStar = (projectId) => {
+        fetch('/api/projects/modify_star/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify({
+                project_id: projectId
+            }),
+        }).then(res => res.json())
+        .then(
+            (r) => {
+                console.log(r);
             },
             (error) => {
                 console.log(error);
             }
         )
-    }, [])
 
-    if (!user) return go('/api/account/login/google/?next=/account');
+        setProjects(projects.filter((p) => p.id !== projectId))
+    }
 
-    if (!user.username) return <div></div>;
+
+    const logout = () => {
+        fetch('/api/account/logout/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+        })
+        .then(res => res.json())
+        .then(
+            (r) => {
+                if (r.success) {
+                    go('/')
+                    // history.push('/')
+                } else {
+                    console.log(r.Error);
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+
+
+    if (!user) { go('/login');return <></>; } else if (!user.username) return <></>
+
 
     if (user.picture) {
         if (user.picture.slice(-5) === 's96-c') {
             user.picture = user.picture.slice(0,-5) + 's500-c'
         }
     }
-
     
     return (
         <div className='account'>
@@ -65,7 +117,7 @@ const Account = () => {
                     <span> <FiHexagon /> {user.nickname} </span>
                     <span> <FiUser /> {user.username} </span>
                     <span> <FiAtSign /> {user.email} </span>
-                    <span> <FiStar /> {user.stared_projects.length} </span>
+                    <span> <FiStar /> {projects.length} </span>
 
                     <div className='actions'>
                         <Button onClick={() => {}}>Edit</Button>
@@ -76,9 +128,7 @@ const Account = () => {
             </div>
             <div className='started-projects'>
                 <span className='title'>Stared Projects</span>
-                {user.stared_projects.map((p, i) => {
-                    return <div key={i} className='project'></div>
-                })}
+                <Projects projects={projects} Rstar={removeStar} history={history} />
             </div>
         </div>
     )
