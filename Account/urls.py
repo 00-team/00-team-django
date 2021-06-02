076b3594a4a7bcc3dd5ad74f9a5b79cb4c312071
i007c <dr007cc@gmail.com>
2021-06-03 02:58:02 +0430

    better name for view funcs

--- Account/urls.py
+++ Account/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.account, name='account view'),
-    path('login/', views.login_user, name='Login'),
+    path('login/', views.login, name='Login'),
     path('login/google/', views.authorize, name='google authorize'),
     path('login/google_callback/', views.google_callback, name='google callback'),
-    path('logout/', views.logout_user, name='logout'),
+    path('logout/', views.logout, name='logout'),
     path('change_password/', views.change_password, name='Change Password'),
     path('change_info/', views.change_info, name='Change Informarion'),
 ]
