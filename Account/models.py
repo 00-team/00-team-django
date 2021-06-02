f7239f6dd9b92f55605a5847f7b5ee95d9bb8231
i007c <dr007cc@gmail.com>
2021-06-03 02:57:22 +0430

    better filds

--- Account/models.py
+++ Account/models.py
@@ -8,14 +8,13 @@ import string
 
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=100, default="No Name")
+    nickname = models.CharField(max_length=100, blank=True)
     picture = models.URLField(null=True, blank=True)
-    locale = models.CharField(max_length=10)
     token = models.CharField(max_length=50, null=True, blank=True, unique=True)
 
     def change_token(self):
         try:
-            self.token = get_random_string(50, string.ascii_letters + string.digits + "!@#$%^&*")
+            self.token = get_random_string(50, string.ascii_letters + string.digits + '!@#$%^&*')
             self.save()
         except IntegrityError:
             self.change_token()
@@ -26,4 +25,4 @@ class UserAccount(models.Model):
         super(UserAccount, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.nickname
+        return self.nickname or str(self.user)
