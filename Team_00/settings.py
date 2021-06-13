fff76a0c2b5b0b9eaa13bc7058d60f6c093e28ad
i007c <dr007cc@gmail.com>
2021-06-14 01:39:19 +0430

    add email configs

--- Team_00/settings.py
+++ Team_00/settings.py
@@ -104,3 +104,11 @@ MEDIA_URL = '/m/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
