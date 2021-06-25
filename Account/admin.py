0568eebe550c263998a3c6c70c87c2117652fe0c
i007c <dr007cc@gmail.com>
2021-06-25 15:32:57 +0430

    better admin...

--- Account/admin.py
+++ Account/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import UserAccount, UserTemp
 
-admin.site.register(UserAccount)
-admin.site.register(UserTemp)
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nick', 'email')
+    list_per_page = 15
+
+    @admin.display(description='Nickname')
+    def nick(self, obj):
+        return obj.nickname or 'No Name'
+    
+
+    @admin.display
+    def email(self, o):
+        return o.user.email or 'No Mail'
+
+
+@admin.register(UserTemp)
+class UserTempAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'code', 'check_time')
+    list_per_page = 15
--- Projects/admin.py
+++ Projects/admin.py
@@ -2,7 +2,35 @@ from django.contrib import admin
 
 from .models import Project, Star, DocumentVideos, DocumentImages
 
-admin.site.register(Project)
-admin.site.register(Star)
-admin.site.register(DocumentVideos)
-admin.site.register(DocumentImages)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'language', 'workspace')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('date_start', 'status', 'slug', 'git'),
+        }),
+    )
+
+    list_filter = ('status', 'language', 'workspace')
+    list_per_page = 15
+
+
+@admin.register(DocumentVideos, DocumentImages)
+class DocumentAdmin(admin.ModelAdmin):
+    list_per_page = 15
+    list_display = ('__str__', 'status')
+
+    @admin.display
+    def status(self, o):
+        s = o.project.status or 'No Status'
+        return ('Public' if o.project.status == 'PB' else 'Private')
+
+@admin.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    list_per_page = 15
+    list_display = ('user', 'project')
+    list_filter = ('user', 'project')
