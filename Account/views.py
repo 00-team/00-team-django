52ef3ec9cab4e2043f53acfa701163d05f577f28
i007c <dr007cc@gmail.com>
2021-07-07 01:45:37 +0430

    move other functions

--- Account/views.py
+++ Account/views.py
@@ -20,15 +20,11 @@ from django.middleware.csrf import get_token
 
 from Account.models import UserAccount, UserTemp
 from Projects.models import Project, Star
-from Projects.functions import GetThumbnail
+from Projects.functions import *
 
 from .decorators import login_required
 
 GOOGLE = settings.GOOGLE
-
-def CheckTmeps():
-    for ut in UserTemp.objects.all():
-        ut.check_time
 
 def ValidPassword(password) -> str:
     if password:
@@ -49,45 +45,6 @@ def ValidUsername(username) -> str:
                             return None
                     return username
     return None
-
-def GetOrMakeUA(user: User) -> UserAccount:
-    try:
-        return UserAccount.objects.get(user=user)
-    except UserAccount.DoesNotExist:
-        ua = UserAccount(user=user)
-        ua.save()
-        return ua
-
-def BodyLoader(body):
-    try:
-        return json.loads(body)
-    except Exception:
-        return {}
-
-def NextPath(r):
-    data = {}
-
-    if r.method == 'GET':
-        data = r.GET
-    elif r.method == 'POST':
-        data = r.POST
-    elif r.body:
-        data = BodyLoader(r.body)
-    
-    if data.get('next'):
-        return data.get('next')
-    elif r.session.get('next'):
-        return r.session.get('next')
-    
-    return '/'
-
-def RequestData(response):
-    try:
-        return response.json()
-    except Exception:
-        return {
-            'error': 'can`t get data'
-        }
 
 def googleRedirect(r):
     return f'{r.scheme}://{r.get_host()}' + GOOGLE['redirect_uri']
--- Projects/functions.py
+++ Projects/functions.py
@@ -1,7 +1,7 @@
 import json
 
 from .models import Project, Star, DocumentImages, DocumentVideos
-
+from Account.models import UserAccount, User, UserTemp
 
 def GetThumbnail(project: Project) -> str:
     pdv = DocumentVideos.objects.filter(project=project).last()
@@ -14,7 +14,6 @@ def GetThumbnail(project: Project) -> str:
     else:
         return None
 
-
 def BodyLoader(body):
     try:
         return json.loads(body)
@@ -24,10 +23,8 @@ def BodyLoader(body):
 def TOTAL_STARS(p):
     return len(Star.objects.filter(project=p))
 
-
 def USER_STARED(u, p):
     return Star.objects.filter(user=u, project=p).exists()
-
 
 def PROJECT_HAS_VIDEO(p):
     dv = DocumentVideos.objects.filter(project=p).last()
@@ -36,3 +33,40 @@ def PROJECT_HAS_VIDEO(p):
         return dv.video.url
     
     return None
+
+def NextPath(r):
+    data = {}
+
+    if r.method == 'GET':
+        data = r.GET
+    elif r.method == 'POST':
+        data = r.POST
+    elif r.body:
+        data = BodyLoader(r.body)
+    
+    if data.get('next'):
+        return data.get('next')
+    elif r.session.get('next'):
+        return r.session.get('next')
+    
+    return '/'
+
+def RequestData(response):
+    try:
+        return response.json()
+    except Exception:
+        return {
+            'error': 'can`t get data'
+        }
+
+def GetOrMakeUA(user: User) -> UserAccount:
+    try:
+        return UserAccount.objects.get(user=user)
+    except UserAccount.DoesNotExist:
+        ua = UserAccount(user=user)
+        ua.save()
+        return ua
+
+def CheckTmeps():
+    for ut in UserTemp.objects.all():
+        ut.check_time
